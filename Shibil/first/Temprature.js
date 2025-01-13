6680249f1ccdd14cc26805318b4ebfc5d3f48316
shibilca <53700282+shibilca@users.noreply.github.com>
2025-01-13 16:05:31 +0530

    Add files via upload

---
+++ Shibil/first/Temprature.js
@@ -0,0 +1,20 @@
+var person=[{
+    name: "absd",
+    age: 35,
+    city: "London",
+    Occupation: "Manager"
+
+},{
+    name: "svgv",
+    age: 55,
+    city: "delhi",
+    Occupation: "driber"
+
+},{
+    name: "ohv",
+    age: 33,
+    city: "america",
+    Occupation: "doctr"}
+
+]
+console.log(person[2].Occupation)
